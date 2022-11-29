bf59747e74ea899deb6493c97c5ce7bdaf8668ac
Massimo Ruggirello <massimo.ruggirello@foorban.com>
2022-11-29 16:35:19 +0100

    init communication and path logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { CheckName } from './pages/CheckName';
+import { Home } from './pages/Home';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="check-name" element={<CheckName />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/check-name">Check Name</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
     </div>
   );
 }
 
-export default App;
+
+
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
